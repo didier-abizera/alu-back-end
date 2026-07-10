0528174bc0d2bc3b1892d232e5d98092ff0fa542
didier-abizera <d.abizera@alustudent.com>
2026-07-10 16:08:47 +0200

    Update task 0 with working solution

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Script to get todos for a user from API"""
-
 import requests
 import sys
 
@@ -10,21 +9,15 @@ def main():
     user_id = int(sys.argv[1])
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
     user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-
     response = requests.get(todo_url)
-
     total_questions = 0
     completed = []
     for todo in response.json():
-
         if todo['userId'] == user_id:
             total_questions += 1
-
             if todo['completed']:
                 completed.append(todo['title'])
-
     user_name = requests.get(user_url).json()['name']
-
     printer = ("Employee {} is done with tasks({}/{}):".format(user_name,
                len(completed), total_questions))
     print(printer)
